02774c9db517685c6f183505922f0a1b368c3f97
Frank <frankaconiglio@outlook.com>
2021-12-27 18:00:58 -0500

    Adding Writer File, Starting Inv Report sorting

--- Ezra/Ez.py
+++ Ezra/Ez.py
@@ -7,12 +7,26 @@ from tkinter.constants import COMMAND
 from subprocess import STDOUT, Popen
 import subprocess
 
+#declaring writer variable to output multiple sheets into on document
+
+writer = pd.ExcelWriter(r'V:\code\seal-team\Ezra\output.xlsx', engine='xlsxwriter')
+
 # Declaring and requesting user input for a backlog report and inv report
 
 backlog = filedialog.askopenfilename(initialdir='/', title = "Select Backlog Report", filetypes=(("All Files","*.*"),("Excel","*.xls")))
 inv_report = filedialog.askopenfilename(initialdir='/', title = "Select Inventory Report", filetypes=(("All Files","*.*"),("Excel","*.xls")))
 
-#printing out to check path
-print(backlog)
-print(inv_report)
 
+#declaring data frames for both reports
+
+backlog_df = pd.read_excel(backlog,index_col=False)
+
+inv_df = pd.read_excel(inv_report,index_col=False)
+
+SKU = input("Enter Exact SKU Name: ")
+
+#Data Frame to Sort Inventory Report By BOM (Highest First)
+
+inv_bom_sort = inv_df.sort_values(by=['BOM'], ascending=False)
+inv_bom_sort.to_excel(writer,sheet_name='Inv_DF',index=False)
+writer.save()
