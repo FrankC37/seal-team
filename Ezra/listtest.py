1230325eb085db04ff8ede4bd94a75eb2c13376c
frank@usasealing <frank.coniglio@usasealing.com>
2021-12-28 13:22:15 -0500

    looping file working

---
+++ Ezra/listtest.py
@@ -0,0 +1,10 @@
+
+user_input = input("Enter SKUs: ")
+
+skus = user_input.split(",")
+
+
+for x in skus:
+    print(x)
+    
+    
--- Ezra/tempCodeRunnerFile.py
+++ Ezra/tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
+user_input = input("Enter SKU: ")
+skus = user_input.split(", ")
 
+print(skus)
