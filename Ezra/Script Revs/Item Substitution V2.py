4635bc964aada22bdc59b8d6825608a66a2a3469
frank@usasealing <frank.coniglio@usasealing.com>
2021-12-29 10:56:49 -0500

    V2 is a workign copy now, sorting is fixed

---
+++ Ezra/Script Revs/Item Substitution V2.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from tqdm import tqdm
+from datetime import datetime
+import os
+
+inv=pd.read_excel('C:/Users/fconiglio/git/seal-team/Ezra/EZProductionInvSearchResults752.xlsx')
+sales=pd.read_excel('C:/Users/fconiglio/git/seal-team/Ezra/EZ_Backlog_by_Product470.xlsx')
+
+# Date Conversions
+sales["Date"] = pd.to_datetime(sales["Date"])
+sales["Date"] = sales["Date"].dt.strftime('%m/%d/%Y')
+sales["Requested Ship Date"] = pd.to_datetime(sales["Requested Ship Date"])
+sales["Requested Ship Date"] = sales["Requested Ship Date"].dt.strftime('%m/%d/%Y')
+#Output File
+df= r"C:/Users/fconiglio/git/seal-team/Ezra/output/"
+
+#Taking user input for Multiple SKUS, must be Comma Seperated
+user_input = input("Enter SKU: ")
+#Splitting the input into a list delimited by commas per item
+skus = user_input.split(",")
+
+#Loop to run through all SKUs in list, using TQDM for progress bar
+for sku in tqdm(skus):
+    #creating a new dataframe and pulling in matching sku data from inv file
+    child_info=inv[inv['Item']==sku] 
+    
+    #creating a new dataframe with matching rm parents from the inv file
+    # the number 4 refers to the column position, needs to updated if inv files is changed
+    inv_children=inv[inv['RM Master Parent']==child_info.iat[0,4] ]
+    
+    #creating a new data set by pulling out specified field of the sku in question we like to look at on top - output 0
+    child_info_stack=child_info[['Item','RM Length','RM Width']] 
+
+    #output 1
+    inv_children=inv_children[['Item','RM Thickness','RM Length','RM Width','On Hand','On Order','Committed','Preferred Stock Level','Reorder Point','Reorder Multiple','RM Master Parent','BOM','Bin Number']] 
+
+    #pulling in sales againt the children, has duplicates
+    sales_match=pd.merge(left=child_info, right=sales,how='left',left_on='Item', right_on='Product SKU') 
+
+    #dropping excess columns/redefining the data set, output 2
+    sales_match=sales_match[['Product SKU','Document Number','Date','Qty on Backorder','Qty on Order','Qty Available','Requested Ship Date']] 
+
+    
+    #testing if this works, dropping indexing off the dataframe
+    children_sales_grouped=pd.concat([inv_children.reset_index(drop=True), sales_match], axis= 1) 
+    
+    # organizing by bom
+    result_wip = children_sales_grouped.sort_values(by='BOM',ascending=False) 
+    
+    #adding the sku in quesiton info to our over all results
+    result= pd.concat([child_info_stack.reset_index(drop=True), result_wip.reset_index(drop=True)], axis= 1)
+
+    #printing each SKU into its own Excel document to the hardcoded path
+    result.to_excel(df + sku + ".xlsx",index= False)
+    #declaring path to file we just output
+    file = df+sku+'.xlsx'
+    #opening file in excel
+    os.startfile(file)
+print("File(s) Ready") #decorations
--- Ezra/tempCodeRunnerFile.py
+++ Ezra/tempCodeRunnerFile.py
@@ -1,4 +1,7 @@
-user_input = input("Enter SKU: ")
-skus = user_input.split(", ")
 
-print(skus)
+    sales_match=sales_match[['Product SKU','Document Number','Date','Qty on Backorder','Qty on Order','Qty Available','Requested Ship Date']] #dropping excess columns/redefining the data set, output 2
+
+    #children_sales_grouped=pd.concat([inv_children, sales_match], axis= 1) #chilren sku with related sales pulled in their own columns, takes care of duplicates as they get their own column and by doing sales_match I only captured the relevant ones
+    
+    children_sales_grouped=pd.concat([inv_children.reset_index(drop=True), sales_match], axis= 1) 
+    #testing if this works, dropping indexing off the dataframe
